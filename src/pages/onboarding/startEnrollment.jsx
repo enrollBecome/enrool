8907ac4f59d900cbb834a542e21383cad9a16028
Rishab Jain <rjain22061998@gmail.com>
2025-01-06 16:53:41 +0530

    mail for initial sign up

--- src/pages/onboarding/startEnrollment.jsx
+++ src/pages/onboarding/startEnrollment.jsx
@@ -307,15 +307,17 @@ const StartEnrollment = () => {
             console.error("Error sending email:", errorData);
             return;
           }else{
+            
+          }
+
+          const data = await response.json();
+          if(data){
             toast({
               title: "Uh oh! Something went wrong.",
               description:  "There was a problem with your request.",
               action: <ToastAction altText="Try again">Try again</ToastAction>,
             });
           }
-
-          const data = await response.json();
-          console.log(data)
         };
 
         // Usage
