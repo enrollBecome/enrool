02e0bb25ddf24d1fe7c7d4a96569a1c148993850
Rishab Jain <rjain22061998@gmail.com>
2025-02-07 10:03:37 +0530

    first commit

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -10,7 +10,7 @@ const LandingPage = () => {
     </div>
     <div className='md:p-20 sm:p-4 flex sm:gap-4 md:gap-20 justify-center sm:flex-col sm:items-center md:flex-row'>
         <img src="https://tallkizetxyhcvjujgzw.supabase.co/storage/v1/object/public/uploads/deanimage.png" className='rounded-full md:w-[550px] md:h-[530px] sm:w-4/5' />
-        <div className='sm:w-5/6 md:w-1/2 '>
+        <div className='sm:w-[95%] md:w-1/2 '>
         <h1 className='poppins-regular text-lg pb-6'>Dear Student,</h1>
         <p className='poppins-regular text-lg leading-normal text-justify pb-4'>
             It gives me immense pleasure to welcome you to our registration page for the {" "}
